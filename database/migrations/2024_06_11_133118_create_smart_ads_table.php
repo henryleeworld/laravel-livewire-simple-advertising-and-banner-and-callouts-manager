aa1e5b78e85bcfff0126ebcd17aa2e4ba428a458
Henry Lee <henryleeworld@gmail.com>
2024-06-20 00:02:31 +0800

    feat:升級至 laravel 11

--- database/migrations/2024_06_11_133118_create_smart_ads_table.php
+++ database/migrations/2024_06_11_133118_create_smart_ads_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSmartAdsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('smart_ads', function (Blueprint $table) {
             $table->id();
@@ -32,11 +30,9 @@ class CreateSmartAdsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('smart_ads');
     }
-}
+};
--- database/migrations/2024_06_11_133119_create_smart_ads_tracking_table.php
+++ database/migrations/2024_06_11_133119_create_smart_ads_tracking_table.php
@@ -4,14 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSmartAdsTrackingTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('smart_ads_tracking', function (Blueprint $table) {
             $table->id();
@@ -23,11 +21,9 @@ class CreateSmartAdsTrackingTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('smart_ads_tracking');
     }
-}
+};
